7819c6e49bdedbe8a3cbecaf4ab9c23ee159cf23
Farazur Rahman <farazkhanmba@gmail.com>
2026-04-26 13:45:41 +0530

    fix: lint formatting issues

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/health')
+@app.route("/health")
 def health():
-    return 'Server is up and running'
-    
+    return "Server is up and running"
+
