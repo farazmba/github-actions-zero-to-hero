3d585c7f0a514edbf8b9d29e8d27c2f7e00b7d04
Farazur Rahman <farazkhanmba@gmail.com>
2026-04-26 18:47:41 +0530

    linting

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
-from flask import Flask, render_templateapp = Flask(__name__)@app.route("/")def hello_world():    return render_template("index.html")@app.route("/health")def health():    return "Server is up and running"
+from flask import Flask, render_template
+app = Flask(__name__)
 
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/health')
+def health():
+    return 'Server is up and running'
